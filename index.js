5ccbe09c5a1fa422e4fab8cc0159dcec4349b656
Abhishek Shah <ab.shah15@gmail.com>
2023-10-20 18:44:09 +0530

    Add some db connection and one db task

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser"); 
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ 
+  extended: true
+})); 
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+
+mongoose.connect("mongodb://localhost:27017/test", {useNewUrlParser: true});
+
+const contactSchema = {
+  email: String,
+  query: String,
+}; 
+const Contact = mongoose.model("Contact", contactSchema);
+
+app.post("/contact", function (req, res) {
+  const contact = new Contact({
+      email: req.body.email,
+      query: req.body.query,
+  });
+  contact.save(function (err) {
+      if (err) {
+        res.send("Error Occurred");
+      } else {
+        res.send("Contact Save");
+      }
+  });
+});
 
 app.get('/', function (req, res) {
   res.send('Hello Docker')
